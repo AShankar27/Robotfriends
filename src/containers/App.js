7aa42b232f025a8f5e3e8167a935b24951167280
Abhishek <abhi.ashwin@gmail.com>
2021-02-26 21:59:53 +0530

    minor updates

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,12 +15,9 @@ class App extends Component {
 	}
 
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-			return response.json();
-		})
-		.then(users => {
-			this.setState({robots: users})
-		});
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => {return response.json()})
+			.then(users => {this.setState({robots: users})});
 	}
 
 	onSearchChange = (event) => {
@@ -28,18 +25,22 @@ class App extends Component {
 	}
 
 	render() {
-		const filteredRobots = this.state.robots.filter(robots => {
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robots => {
+			return robots.name.toLowerCase().includes(searchfield.toLowerCase())
 		})
-		return (
-			<div className = 'tc'>
-				<h1 className='f1'>Robofriends</h1>
-				<SearchBox searchChange = {this.onSearchChange}/>
-				<Scroll>
-					<CardList robots={filteredRobots}/>
-				</Scroll>
-			</div>
-		);
+		return !robots.length ?
+			<h1>Loading</h1> : 
+			(
+				<div className = 'tc'>
+					<h1 className='f1'>Robofriends</h1>
+					<SearchBox searchChange = {this.onSearchChange}/>
+					<Scroll>
+						<CardList robots={filteredRobots}/>
+					</Scroll>
+				</div>
+			);
+		
 	}
 }
 
